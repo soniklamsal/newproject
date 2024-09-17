936ee215a20519e98932256ad6e4e145060aabd4
newproject-demo <soniklamsal111@gmail.com>
2024-09-17 16:15:05 +0545

    deploy

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import Alert from './components/Alert';
 import React, { useState } from 'react';
-// import {
-//   Routes,
-//   Route
-// } from 'react-router-dom';
+import {
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
   const [mode, setMode] = useState('success');
@@ -40,13 +40,13 @@ function App() {
       <NavBar title="TextUtils" aboutText="About TextUtils" mode={mode} />
       <Alert alert={alert} />
       {/* <button className='btn btn-primary' onClick={pickColor}>Click here</button> */}
-      <TextForm />
-      {/* <Routes>
+      {/* <TextForm /> */}
+      <Routes>
         <Route exact path="/textform" element={<TextForm />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/textform" element={<TextForm />} />
         <Route exact path="/navbar" element={<NavBar title="TextUtils" aboutText="About TextUtils" mode={mode} />} />
-      </Routes> */}
+      </Routes>
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function NavBar({ title = "TextUtilsD", aboutText = "About UsD", mode = "light" }) {
   return (
     <nav className={`navbar bg-${mode}`} data-bs-theme="dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">{title}</a> {/* Use Link instead of a tag */}
+        <Link className="navbar-brand" to="/">{title}</Link> {/* Use Link instead of a tag */}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" href="#">Home</a>
+              <Link className="nav-link" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" to="#">{aboutText}</a>
+              <Link className="nav-link" to="/about">{aboutText}</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">textform</a>
+              <Link className="nav-link" to="/textform">textform</Link>
             </li>
           </ul>
           <form className="d-flex" role="search">
